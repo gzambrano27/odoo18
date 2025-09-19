bbdc581434e9528913971b5a7e8550cebde22560
gzambrano27 <gzambrano@gpsgroup.com.ec>
2025-09-19 14:18:29 -0500

    eliminacion de modulo de wc_chatgpt_integration

--- extra_addons/customaddons-main/wc_chatgpt_integration/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Wicoders Solutions. See LICENSE file for full copyright and licensing details
-
-from . import models
--- extra_addons/customaddons-main/wc_chatgpt_integration/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Wicoders Solutions. See LICENSE file for full copyright and licensing details
-
-from . import mail_channel
-from . import res_config_settings
